f7ad989c091c92f76a6809067e06c34ade8e6e31
Abraham Chen <happyabbi@gmail.com>
2016-04-18 23:23:39 +0800

    init

---
+++ POS/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace POS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewOrder = new System.Windows.Forms.DataGridView();
+            this.dataGridViewMenu = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonName = new System.Windows.Forms.Button();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.numericUpDownNumber = new System.Windows.Forms.NumericUpDown();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonTotals = new System.Windows.Forms.Button();
+            this.textBoxTotals = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewOrder
+            // 
+            this.dataGridViewOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrder.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnName,
+            this.ColumnPrice,
+            this.ColumnNumber,
+            this.ColumnTotal});
+            this.dataGridViewOrder.Location = new System.Drawing.Point(12, 139);
+            this.dataGridViewOrder.Name = "dataGridViewOrder";
+            this.dataGridViewOrder.Size = new System.Drawing.Size(422, 295);
+            this.dataGridViewOrder.TabIndex = 0;
+            // 
+            // dataGridViewMenu
+            // 
+            this.dataGridViewMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMenu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridViewMenu.Location = new System.Drawing.Point(440, 12);
+            this.dataGridViewMenu.Name = "dataGridViewMenu";
+            this.dataGridViewMenu.Size = new System.Drawing.Size(240, 422);
+            this.dataGridViewMenu.TabIndex = 1;
+            this.dataGridViewMenu.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMenu_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "品名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(279, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "小計";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "數量";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "單價";
+            // 
+            // buttonName
+            // 
+            this.buttonName.Location = new System.Drawing.Point(12, 48);
+            this.buttonName.Name = "buttonName";
+            this.buttonName.Size = new System.Drawing.Size(75, 23);
+            this.buttonName.TabIndex = 6;
+            this.buttonName.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(93, 48);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(74, 20);
+            this.textBoxPrice.TabIndex = 7;
+            // 
+            // numericUpDownNumber
+            // 
+            this.numericUpDownNumber.Location = new System.Drawing.Point(177, 48);
+            this.numericUpDownNumber.Name = "numericUpDownNumber";
+            this.numericUpDownNumber.Size = new System.Drawing.Size(73, 20);
+            this.numericUpDownNumber.TabIndex = 8;
+            this.numericUpDownNumber.ValueChanged += new System.EventHandler(this.numericUpDownNumber_ValueChanged);
+            // 
+            // textBoxTotal
+            // 
+            this.textBoxTotal.Location = new System.Drawing.Point(272, 48);
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.Size = new System.Drawing.Size(74, 20);
+            this.textBoxTotal.TabIndex = 9;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(352, 46);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "加入";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.HeaderText = "品名";
+            this.ColumnName.Name = "ColumnName";
+            // 
+            // ColumnPrice
+            // 
+            this.ColumnPrice.HeaderText = "單價";
+            this.ColumnPrice.Name = "ColumnPrice";
+            // 
+            // ColumnNumber
+            // 
+            this.ColumnNumber.HeaderText = "數量";
+            this.ColumnNumber.Name = "ColumnNumber";
+            // 
+            // ColumnTotal
+            // 
+            this.ColumnTotal.HeaderText = "小計";
+            this.ColumnTotal.Name = "ColumnTotal";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "品名";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "單價";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // buttonTotals
+            // 
+            this.buttonTotals.Location = new System.Drawing.Point(352, 440);
+            this.buttonTotals.Name = "buttonTotals";
+            this.buttonTotals.Size = new System.Drawing.Size(75, 23);
+            this.buttonTotals.TabIndex = 11;
+            this.buttonTotals.Text = "總計";
+            this.buttonTotals.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTotals
+            // 
+            this.textBoxTotals.Location = new System.Drawing.Point(246, 442);
+            this.textBoxTotals.Name = "textBoxTotals";
+            this.textBoxTotals.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTotals.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 475);
+            this.Controls.Add(this.textBoxTotals);
+            this.Controls.Add(this.buttonTotals);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTotal);
+            this.Controls.Add(this.numericUpDownNumber);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.buttonName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewMenu);
+            this.Controls.Add(this.dataGridViewOrder);
+            this.Name = "Form1";
+            this.Text = "POS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewOrder;
+        private System.Windows.Forms.DataGridView dataGridViewMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonName;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDownNumber;
+        private System.Windows.Forms.TextBox textBoxTotal;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Button buttonTotals;
+        private System.Windows.Forms.TextBox textBoxTotals;
+    }
+}
+
---
+++ POS/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataGridViewRowCollection rows = dataGridViewMenu.Rows;
+            rows.Add(new Object[] {"紅茶",25 });
+            rows.Add(new Object[] { "綠茶", 25 });
+            rows.Add(new Object[] { "奶茶", 30 });
+            rows.Add(new Object[] { "珍珠奶茶", 35 });
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewMenu_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewMenu.Rows[e.RowIndex].Cells[0].Value == null) {
+                return;
+            }
+
+            buttonName.Text = dataGridViewMenu.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBoxPrice.Text = dataGridViewMenu.Rows[e.RowIndex].Cells[1].Value.ToString();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            subtotal();
+            dataGridViewOrder.Rows.Add(new Object[] { name, price, number, sum });
+            calculate();
+        }
+
+        string name;
+        double price;
+        double number;
+        double sum;
+
+        private void subtotal() {
+             name = buttonName.Text;
+             price = double.Parse(textBoxPrice.Text);
+             number = (double)numericUpDownNumber.Value;
+             sum = price * number;
+            textBoxTotal.Text = sum.ToString();
+
+        }
+
+        private void numericUpDownNumber_ValueChanged(object sender, EventArgs e)
+        {
+            subtotal(); 
+        }
+
+        private void calculate() {
+            double sum = 0.0;
+
+            for (int i = 0; i < dataGridViewOrder.Rows.Count; i++) {
+                DataGridViewRow row = dataGridViewOrder.Rows[i];
+                if(row.Cells[0].Value!= null)
+                {
+                    sum = sum + (double)row.Cells[3].Value;
+                }
+                textBoxTotals.Text = sum.ToString();
+            }
+
+        }
+    }
+}
